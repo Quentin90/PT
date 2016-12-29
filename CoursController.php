7ff1c8349a0c7bbb239271aea18a33a0e44a1a1a
Alturnox <hugo.yes@hotmail.fr>
2016-12-29 18:26:46 +0100

    Add files via upload

--- CoursController.php
+++ CoursController.php
@@ -1,74 +1,170 @@
 <?php
 namespace App\Controller;
-
-use App\Model\UserModel;
-
+use App\Model\CommandeModel;
 use Silex\Application;
-use Silex\Api\ControllerProviderInterface;   // modif version 2.0
-
+use Silex\ControllerProviderInterface;
 use Symfony\Component\HttpFoundation\Request;   // pour utiliser request
-
-class CoursController implements ControllerProviderInterface {
-
-    private $CoursModel;
-
+use App\Model\CoursModel;
+use Symfony\Component\Validator\Constraints as Assert;   // pour utiliser la validation
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Security;
+class CoursController implements ControllerProviderInterface
+{
+    public function __construct()
+    {
+    }
     public function index(Application $app) {
-        return $this->connexionCours($app);
+        return $this->show($app);
     }
-
-    public function showCours(Application $app){
-        $this->CoursModel=new CoursModel($app);
-        $donnees=$this->CoursModel->showCours();
-
-        return $app["twig"]->render('backOff/Cours/show.html.twig',['donnees'=>$donnees]);
-
+    public function show(Application $app) {
+        $this->CoursModel = new CoursModel($app);
+        $Cours = $this->CoursModel->getAllCours();
+        return $app["twig"]->render('backOff/Cours/homepage.html.twig',['data'=>$Cours]);
     }
-
-    public function addCours(Application $app){
-        $this->CoursModel=new CoursModel($app);
-        return $app["twig"]->render('backOff/Cours/add.html.twig');
-
+    public function add(Application $app) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
+        $this->CoursModel = new CoursModel($app);
+        $Cours = $this->CoursModel->getAllCours();
+        return $app["twig"]->render('backOff/Cours/add.html.twig',['Cours'=>$Cours,'path'=>BASE_URL]);
+        return "add Cours";
     }
-    public function validFormAddCours(Application $app, Request $req) {
+    public function validFormAdd(Application $app, Request $req) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
         // var_dump($app['request']->attributes);
-        if (isset($_POST['id']) && isset($_POST['nom']) and isset($_POST['description']) and isset($_POST['id_user'])and isset($_POST['id_matiere'])){
+        if (isset($_POST['nom_cours']) && isset($_POST['description_cours']) and isset($app["session"]) and isset($_POST['id_matiere'])) {
             $donnees = [
-                'id' => htmlspecialchars($_POST['id']),                    // echapper les entrées
-                'nom' => htmlspecialchars($_POST['nom']),
-                'description' => htmlspecialchars($_POST['description']),
-                'id_user' => htmlspecialchars($_POST['id_user']),
-                'id_matiere' => htmlspecialchars($_POST['id_matiere'])
+                'nom_cours' => htmlspecialchars($_POST['nom_cours']),                    // echapper les entrées
+                'description_cours' => htmlspecialchars($req->get('description_cours')),
+                'id_user' => $app ["session"]->get("id_user"))
+                'id_matiere' => $app->escape($req->get('id_matiere'))
             ];
-
-
+            if(! preg_match("/^[A-Za-z ]{2,}/",$donnees['nom_cours'])) $erreurs['nom_cours']='Doit être composé de 2 lettres minimum';
+            if(! preg_match("/^[A-Za-z ]{10,}/",$donnees['description_cours'])) $erreurs['description_cours']='Doit être composé de 10 lettres minimum';
+            if(! is_numeric($donnees['id_matiere']))$erreurs['id_matiere']='Veuillez saisir une matière'; //Liste déroulante
             if(! empty($erreurs))
             {
                 $this->CoursModel = new CoursModel($app);
-                return $app["twig"]->render('backOff/Cours/add.html.twig',['donnees'=>$donnees,'erreurs'=>$erreurs]);
+                $Cours = $this->CoursModel->getAllCours();
+                return $app["twig"]->render('backOff/Cours/add.html.twig',['donnees'=>$donnees,'erreurs'=>$erreurs,'Cours'=>$Cours]);
             }
             else
             {
                 $this->CoursModel = new CoursModel($app);
-                $this->CoursModel->addCours($donnees);
-                var_dump($donnees);
-                return $app->redirect($app["url_generator"]->generate("Cours.showCours"));
+                $this->CoursModel->insertCours($donnees);
+                return $app->redirect($app["url_generator"]->generate("Cours.index"));
             }
-
         }
         else
-
             return $app->abort(404, 'error Pb data form Add');
     }
-
-
-    public function connect(Application $app) {
+    public function delete(Application $app, $id) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
+        $this->CoursModel = new CoursModel($app);
+        $Cours = $this->CoursModel->getAllCours();
+        $this->CoursModel = new CoursModel($app);
+        $donnees = $this->CoursModel->getCours($id);
+        return $app["twig"]->render('backOff/Cours/delete.html.twig',['Cours'=>$Cours,'donnees'=>$donnees]);
+        return "add Cours";
+    }
+    public function validFormDelete(Application $app, Request $req) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
+        $id=$app->escape($req->get('id_cours'));
+        if (is_numeric($id_cours)) {
+            $this->CoursModel = new CoursModel($app);
+            $this->CoursModel->deleteCours($id_cours);
+            return $app->redirect($app["url_generator"]->generate("Cours.index"));
+        }
+        else
+            return $app->abort(404, 'error Pb id form Delete');
+    }
+    public function edit(Application $app, $id) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
+        $this->CoursModel = new CoursModel($app);
+        $Cours = $this->CoursModel->getAllCours();
+        $this->CoursModel = new CoursModel($app);
+        $donnees = $this->CoursModel->getCours($id);
+        return $app["twig"]->render('backOff/Cours/edit.html.twig',['Cours'=>$Cours,'donnees'=>$donnees]);
+        return "add Cours";
+    }
+    public function validFormEdit(Application $app, Request $req) {
+        if(isset($app["session"])&&$app ["session"]->get("droit")!="DROITadmin"){
+            return "Vous n'avez pas les droits";
+        }
+        // var_dump($app['request']->attributes);
+        if (isset($_POST['nom_cours']) && isset($_POST['description_cours']) and isset($app["session"]) and isset($_POST['id_matiere'])) {
+            $donnees = [
+                'nom_cours' => htmlspecialchars($_POST['nom_cours']),                    // echapper les entrées
+                'description_cours' => htmlspecialchars($req->get('description_cours')),
+                'id_user' => $app ["session"]->get("id_user"))
+                'id_matiere' => $app->escape($req->get('id_matiere'))
+            ];
+            if(! preg_match("/^[A-Za-z ]{2,}/",$donnees['nom_cours'])) $erreurs['nom_cours']='Doit être composé de 2 lettres minimum';
+            if(! preg_match("/^[A-Za-z ]{10,}/",$donnees['description_cours'])) $erreurs['description_cours']='Doit être composé de 10 lettres minimum';
+            if(! is_numeric($donnees['id_matiere']))$erreurs['id_matiere']='Veuillez saisir une matière'; //Liste déroulante
+            $contraintes = new Assert\Collection(
+                [
+                    'id_cours' => [new Assert\NotBlank(),new Assert\('digit')],
+                    'nom_cours' => [
+                        new Assert\NotBlank(['message'=>'Entrer le nom du cours']),
+                        new Assert\Length(['min'=>2, 'minMessage'=>"Le nom doit faire au moins {{ limit }} caractères."])
+                    ],
+                    'id_cours' => [new Assert\NotBlank(),new Assert\('digit')],
+                    'description_cours' => [
+                        new Assert\NotBlank(['message'=>'Entrer la description du cours']),
+                        new Assert\Length(['min'=>10, 'minMessage'=>"La description doit faire au moins {{ limit }} caractères."])
+                    ],
+                    'id_matiere' => new Assert\(array(
+                        ''    => 'numeric',
+                        'message' => 'La valeur {{ value }} n\'est pas valide.',
+                    ))
+                ]);
+            $errors = $app['validator']->validate($donnees,$contraintes);  // ce n'est pas validateValue
+            //    $violationList = $this->get('validator')->validateValue($req->request->all(), $contraintes);
+//var_dump($violationList);
+            //   die();
+            if (count($errors) > 0) {
+                // foreach ($errors as $error) {
+                //     echo $error->getPropertyPath().' '.$error->getMessage()."\n";
+                // }
+                // //die();
+                //var_dump($erreurs);
+                // if(! empty($erreurs))
+                // {
+                $this->CoursModel = new CoursModel($app);
+                $Cours = $this->CoursModel->getAllCours();
+                return $app["twig"]->render('backOff/Cours/edit.html.twig',['donnees'=>$donnees,'errors'=>$errors,'erreurs'=>$erreurs,'Cours'=>$Cours]);
+            }
+            else
+            {
+                $this->CoursModel = new CoursModel($app);
+                $this->CoursModel->updateCours($donnees);
+                return $app->redirect($app["url_generator"]->generate("Cours.index"));
+            }
+        }
+        else
+            return $app->abort(404, 'error Pb id form edit');
+    }
+    public function connect(Application $app) {  //http://silex.sensiolabs.org/doc/providers.html#controller-providers
         $controllers = $app['controllers_factory'];
-        $controllers->match('/', 'App\Controller\CoursController::index')->bind('Cours.index');
-        $controllers->get('/showCours', 'App\Controller\CoursController::showCours')->bind('Cours.showCours');
-
-        $controllers->get('/addCours', 'App\Controller\CoursController::addCours')->bind('Cours.addCours');
-        $controllers->post('/validFormAddCours', 'App\Controller\CoursController::validFormAddCours')->bind('Cours.validFormAddCours');
-
+        $controllers->get('/', 'App\Controller\CoursController::index')->bind('Cours.index');
+        $controllers->get('/show', 'App\Controller\CoursController::show')->bind('Cours.show');
+        $controllers->get('/add', 'App\Controller\CoursController::add')->bind('Cours.add');
+        $controllers->post('/add', 'App\Controller\CoursController::validFormAdd')->bind('Cours.validFormAdd');
+        $controllers->get('/delete/{id}', 'App\Controller\CoursController::delete')->bind('Cours.delete')->assert('id', '\d+');;
+        $controllers->delete('/delete', 'App\Controller\CoursController::validFormDelete')->bind('Cours.validFormDelete');
+        $controllers->get('/edit/{id}', 'App\Controller\CoursController::edit')->bind('Cours.edit')->assert('id', '\d+');;
+        $controllers->put('/edit', 'App\Controller\CoursController::validFormEdit')->bind('Cours.validFormEdit');
         return $controllers;
     }
 }
--- CoursModel.php
+++ CoursModel.php
@@ -1,54 +1,71 @@
 <?php
 namespace App\Model;
-
+use Doctrine\DBAL\Query\QueryBuilder;
 use Silex\Application;
-use Doctrine\DBAL\Query\QueryBuilder;;
-
 class CoursModel {
-
     private $db;
-
     public function __construct(Application $app) {
         $this->db = $app['db'];
     }
-
-
-    public function showCours() {
+    // http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/query-builder.html#join-clauses
+    public function getAllCours()
+    {
         $queryBuilder = new QueryBuilder($this->db);
         $queryBuilder
-            ->select('*')
-            ->from('Cours');
+            ->select('c.id_cours', 'c.nom_cours', 'c.description_cours', 'c.id_user', 'c.id_matiere', 'm.nom_matiere')
+            ->from('cours', 'c')
+            ->innerJoin('c', 'matiere', 'm', 'c.id_matiere=m.id_matiere')
+            ->addOrderBy('c.id_matiere', ',c.nom_cours', 'ASC');
         return $queryBuilder->execute()->fetchAll();
-
     }
-
-
-    public function getCours($id_cours) {
+    public function insertCours($donnees) {
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder->insert('cours')
+            ->values([
+                'nom_cours' => '?',
+                'description_cours' => '?',
+                'id_user' => '?',
+                'id_matiere' => '?',
+                'nom_matiere' => '?'
+            ])
+            ->setParameter(0, $donnees['nom_cours'])
+            ->setParameter(1, $donnees['description_cours'])
+            ->setParameter(2, $donnees['id_user'])
+            ->setParameter(3, $donnees['id_matiere'])
+            ->setParameter(4, $donnees['nom_matiere'])
+        ;
+        return $queryBuilder->execute();
+    }
+    function getCours($id) {
         $queryBuilder = new QueryBuilder($this->db);
         $queryBuilder
-            ->select('*')
-            ->from('Cours')
-            ->where('id_cours = :idCours')
-            ->setParameter('idCours', $id_cours);
+            ->select('id_cours', 'nom_cours', 'description_cours', 'id_user', 'id_matiere')
+            ->from('cours')
+            ->where('id_cours= :id_cours')
+            ->setParameter('id_cours', $id_cours);
         return $queryBuilder->execute()->fetch();
-
     }
-    public function addCours($donnees) {
+    public function updateCours($donnees) {
         $queryBuilder = new QueryBuilder($this->db);
-        $queryBuilder->insert('Cours')
-            ->values([
-                'id_cours' => '?',
-                'nom_cours' => '?',
-                'description_cours'=> '?',
-                'id_user' => '?',
-                'id_matiere'=> '?',
-            ])
-            ->setParameter(0, $donnees['id'])
-            ->setParameter(1, $donnees['nom'])
-            ->setParameter(2, $donnees['description'])
-            ->setParameter(3, $donnees['id_user'])
-            ->setParameter(4, $donnees['id_matiere'])
-
+        $queryBuilder
+            ->update('cours')
+            ->set('nom_cours', '?')
+            ->set('description_cours','?')
+            ->set('id_user','?')
+            ->set('id_matiere','?')
+            ->where('id= ?')
+            ->setParameter(0, $donnees['nom_cours'])
+            ->setParameter(1, $donnees['description_cours'])
+            ->setParameter(2, $donnees['id_user'])
+            ->setParameter(3, $donnees['id_matiere']);
+        return $queryBuilder->execute();
+    }
+    public function deleteCours($id) {
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder
+            ->delete('cours')
+            ->where('id_cours = :id_cours')
+            ->setParameter('id_cours',(int)$id_cours)
         ;
         return $queryBuilder->execute();
     }
